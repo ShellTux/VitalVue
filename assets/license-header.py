1a714526950bbd45a16f5f4292e3b7349d0adb96
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-04-10 12:51:00 +0100

    added assets/license-header.py

---
+++ assets/license-header.py
@@ -0,0 +1,24 @@
+################################################################################
+# Project                          ___ ___ __ __         __ ___ ___
+#                                 |   Y   |__|  |_.---.-|  |   Y   .--.--.-----.
+#                                 |.  |   |  |   _|  _  |  |.  |   |  |  |  -__|
+#                                 |.  |   |__|____|___._|__|.  |   |_____|_____|
+#                                 |:  1   |                |:  1   |
+#                                  \:.. ./                  \:.. ./
+#                                   `---'                    `---'
+#
+# Author: João Alves,  2016122878
+# Author: Luís Góis,   2018280716
+# Author: Marco Silva, 2021211653
+#
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+#
+# You may opt to use, copy, modify, merge, publish, distribute and/or sell
+# copies of the Software, and permit persons to whom the Software is
+# furnished to do so, under the terms of the LICENSE file.
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+# KIND, either express or implied.
+#
+################################################################################
