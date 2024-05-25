6467a89c1ddbe05013443cdf5e6151cb9f7127d9
Marco Silva <marcosilva2203@outlook.pt>
2024-05-25 05:28:59 +0100

    7 schedule appointment (#20)
    * add basic functionality to endpoint
    
    * develop sql statement and input values

--- src/app.py
+++ src/app.py
@@ -245,27 +245,64 @@ def user_authentication():
 def schedule_appointment():
     logger.info(f'POST {request.path}')
 
-    token = get_jwt()
-    identity = get_jwt_identity()
+    # 1. get token data
+    id = get_jwt_identity()
+    type = get_jwt().get('type')
+
+    # 2. validate caller
+    if type != IndividualTypes.PATIENT:
+        response = {'status': StatusCode.API_ERROR, 
+                    'errors': 'Only patients can use this endpoint'}
+        return jsonify(response)
+    
+    # 3. get request payload
     payload = request.get_json()
 
+    # 4. query statement and key values
+    statement = """
+                INSERT INTO 
+                    appointment (
+                        doctor_employee_vital_vue_user_id,
+                        scheduled_date,
+                        start_time,
+                        end_time,
+                        patient_vital_vue_user_id
+                    )
+                VALUES (
+                    %s, %s, %s, %s, %s
+                )
+                RETURNING id;
+                """
+    key_values = ['doctor_id', 'date', 
+                  'start_time', 'end_time']
+
+    # 5. validate payload
+    response = validate_payload(payload, key_values)
+    if response:
+        return jsonify(response)
+    
+    # 6. get input values
+    payload['start_time'] = payload['date'] + ' ' + payload['start_time']
+    payload['end_time'] = payload['date'] + ' ' + payload['end_time']
+    input_values = [payload[key] for key in key_values]
+    input_values.append(id)
+
+    # 7. connect to database
     conn = connect_db()
     cursor = conn.cursor()
 
-    statement = ""
-    values = ""
-
     try:
-        cursor.execute(statement, values)
-
-        results = []
+        cursor.execute(statement, input_values)
+        appointment_id = cursor.fetchone()[0]
         response = {'status': StatusCode.SUCCESS.value, 
-                    'results': results}
+                    'results': appointment_id}
+        conn.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(f'POST {request.path} - error: {error}')
         response = {'status': StatusCode.INTERNAL_ERROR.value, 
                     'errors': str(error)}
+        conn.rollback()
 
     finally:
         if conn is not None:
