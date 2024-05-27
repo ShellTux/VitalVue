5e34dcb5c30363fe3b69a0634bf3044f74c76984
Marco Silva <marcosilva2203@outlook.pt>
2024-05-28 00:01:04 +0100

    add function and trigger to associate prescription with patient

--- db-config/03-triggers.sql
+++ db-config/03-triggers.sql
@@ -8,6 +8,11 @@ BEFORE INSERT ON surgery
 FOR EACH ROW
 EXECUTE PROCEDURE create_hospitalization_if_needed();
 
+CREATE TRIGGER trigger_associate_patient_with_prescription
+BEFORE INSERT ON prescription
+FOR EACH ROW
+EXECUTE PROCEDURE associate_patient_with_prescription();
+
 CREATE TRIGGER trigger_create_medicine_if_needed
 BEFORE INSERT ON med_posology
 FOR EACH ROW
